b94312c1b3b53d9156f0da51bfaf12f656485ea1
alari777 <alari777@gmail.com>
2023-04-18 17:38:32 +0300

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require('./config/default');
+
+const app = express();
+app.use(express.json({ extended: true }));
+app.use('/api/v1/auth', require('./routes/auth.route'));
+
+const port = config.port || 3000;
 
 (async () => {
-    const app = express();
-    app.use(express.json());
-
-    const port = process.env.PORT || 3000;
-
+    await mongoose.connect(config.mongoUri);
     app.listen(port, () => {
         console.log("Running in port", port);
     });
