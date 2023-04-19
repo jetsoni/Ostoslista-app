d8af595d5fb8bef7ba65f8a96546870f36869200
alari777 <alari777@gmail.com>
2023-04-19 20:19:19 +0300

    added using of `cors` and `body-parser`

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const config = require('./config/default');
+const config = require("./config/default");
 
 const app = express();
+app.use(cors());
 app.use(express.json({ extended: true }));
-app.use('/api/v1/auth', require('./routes/auth.route'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/v1/auth", require("./routes/auth.route"));
 
 const port = config.port || 5000;
 
 (async () => {
-    await mongoose.connect(config.mongoUri);
-    app.listen(port, () => {
-        console.log("Running in port", port);
-    });
+  await mongoose.connect(config.mongoUri);
+  app.listen(port, () => {
+    console.log("Running in port", port);
+  });
 })();
