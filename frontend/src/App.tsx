82e7a7754d8af3870ff16d6623f7617d0f04d7e0
alari777 <alari777@gmail.com>
2023-04-24 18:52:42 +0300

    added `AuthContext` and `Routing` in App.tsx component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import AuthForm from './components/AuthForm/AuthForm';
+import { AuthContext } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import { useAuth } from './hooks/auth.hook';
+import { useRoutes } from './routes';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const { login, logout, token, userId, userEmail } = useAuth();
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
   return (
-    <div className='App'>
-      <AuthForm />
-    </div>
+    <AuthContext.Provider
+      value={{ login, logout, token, userId, userEmail, isAuthenticated }}
+    >
+      <div className='App'>
+        <BrowserRouter>
+          <Navbar />
+          {routes}
+        </BrowserRouter>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
