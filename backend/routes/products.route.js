166b9af153d1262273ec94c5cb94f12b83399ad0
alari777 <alari777@gmail.com>
2023-04-24 18:49:48 +0300

    imported new middleware for `create product router`; prettier applied its rules

--- backend/routes/products.route.js
+++ backend/routes/products.route.js
@@ -5,59 +5,53 @@ const config = require("../config/default");
 const { check, validationResult } = require("express-validator");
 const Product = require("../models/products");
 const router = Router();
-const ObjectId = require('mongodb').ObjectId;
-
+const ObjectId = require("mongodb").ObjectId;
+const auth = require("../middlewares/auth.middleware");
 
 router.post(
-    "/create",
-    [
-      check("name", "name is too short").isLength({min: 2}),
-      check("amount", "given amount is not a number").isNumeric(),
-    ],
-    async (req, res) => {
-      try {
-        const { errors } = validationResult(req);
-        console.log(errors);
-        if (errors.length !== 0) {
-          return res.status(400).json({
-            errors,
-            message: "wrong register data",
-          });
-        }
-        console.log(req.body);
-        const { name, amount } = req.body;
-  
-        const product = new Product({ name, amount });
-        await product.save();
-  
-        return res.status(201).json({ message: "product was created" });
-      } catch (err) {
-        console.error(`Something goes wrong, ${err.message}`);
-        const { message } = err;
-        return res.status(500).json({ message });
+  "/create",
+  [
+    auth,
+    check("name", "name is too short").isLength({ min: 2 }),
+    check("amount", "given amount is not a number").isNumeric(),
+  ],
+  async (req, res) => {
+    try {
+      const { errors } = validationResult(req);
+      console.log(errors);
+      if (errors.length !== 0) {
+        return res.status(400).json({
+          errors,
+          message: "wrong register data",
+        });
       }
+      console.log(req.body);
+      const { name, amount } = req.body;
+
+      const product = new Product({ name, amount });
+      await product.save();
+
+      return res.status(201).json({ message: "product was created" });
+    } catch (err) {
+      console.error(`Something goes wrong, ${err.message}`);
+      const { message } = err;
+      return res.status(500).json({ message });
     }
-  );
+  }
+);
 
-  router.get(
-    '/product/:id',
-    async (req, res) => {
-        try {
-            const product= await Product.findOne({_id: new ObjectId(req.params.id)});
-            console.log(req.params.id);
-            return res.status(200).json({ productInfo: product})
+router.get("/product/:id", async (req, res) => {
+  try {
+    const product = await Product.findOne({ _id: new ObjectId(req.params.id) });
+    console.log(req.params.id);
+    return res.status(200).json({ productInfo: product });
+  } catch (err) {
+    console.error(`Something goes wrong, ${err.message}`);
+    const { message } = err;
+    return res.status(500).json({ message });
+  }
+});
 
-        } catch (err) {
-          console.error(`Something goes wrong, ${err.message}`);
-          const { message } = err;
-          return res.status(500).json({ message });
-        }
-      }
-    )
+router.delete("/delete/:id");
 
-router.delete(
-    "/delete/:id",
-
-)
-
-module.exports = router
+module.exports = router;
