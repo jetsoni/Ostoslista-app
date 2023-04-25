f6309a9baaeb9f0db7e39b98b0743426aa6dd967
alari777 <alari777@gmail.com>
2023-04-25 11:35:12 +0300

    updated Navbar: new links `Items`, `Recipes` and `Shopping Cart`

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import './navbar.css';
 
 const Navbar = () => {
   const auth = useContext(AuthContext);
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   useEffect(() => {
     setIsLoading(true);
@@ -13,13 +14,21 @@ const Navbar = () => {
   if (isLoading) {
     return (
       <nav>
-        <h1>Ostos Lista</h1>
+        <h1 style={{ cursor: 'pointer' }} onClick={() => navigate('/')}>
+          Ostos Lista
+        </h1>
         {!auth.isAuthenticated && <Link to='/login'>Login</Link>}
         {!auth.isAuthenticated && <Link to='/registration'>Register</Link>}
         {auth.isAuthenticated && (
-          <div style={{ cursor: 'pointer' }} onClick={() => auth.logout()}>
-            Logout (user email: {auth.userEmail})
-          </div>
+          <>
+            <Link to='/items'>Items</Link>
+            <Link to='/recipes'>Recipes</Link>
+            <Link to='/shopping-cart'>Shopping Cart</Link>
+            <div style={{ cursor: 'pointer' }} onClick={() => auth.logout()}>
+              Logout
+            </div>
+            <div>Profile: {auth.userEmail}</div>
+          </>
         )}
       </nav>
     );
