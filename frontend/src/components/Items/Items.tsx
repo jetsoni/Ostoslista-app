ce7fe23b647c0ed01ec3ac9512f25cbba4e3dfea
alari777 <alari777@gmail.com>
2023-04-26 09:20:09 +0300

    updated `Items` component

--- frontend/src/components/Items/Items.tsx
+++ frontend/src/components/Items/Items.tsx
@@ -1,4 +1,12 @@
-import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { filterItemsByRegExp } from '../../helpers/filterItemsByRegExp';
 
 type ItemType = {
   itemName: string;
@@ -10,19 +18,78 @@ const Items: FC = () => {
     itemName: '',
     visible: true,
   });
-
   const [search, setSearch] = useState<string>('');
-
   const [items, setItems] = useState<ItemType[]>([]);
+
+  const getItems = useCallback(async (): Promise<void> => {
+    try {
+      let headers = {};
+      if (localStorage.getItem('userData')) {
+        headers = {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          Authorization:
+            'token ' +
+            JSON.parse(localStorage.getItem('userData') as string).token,
+        };
+      }
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/v1/products`,
+        {
+          method: 'GET',
+          headers,
+        }
+      );
+      if (response.status === 200) {
+        const json = await response.json();
+        console.log(json);
+        const arr = json.items.map((value: { name: string }) => {
+          return {
+            itemName: value.name,
+            visible: true,
+          };
+        });
+        setItems(arr);
+      }
+    } catch (err) {}
+  }, []);
+
+  useEffect(() => {
+    getItems().then();
+  }, [getItems]);
 
   const addItem = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault();
     try {
-      setItems([...items, item]);
-      setItem({
-        itemName: '',
-        visible: true,
-      });
+      let headers = {};
+      if (localStorage.getItem('userData')) {
+        headers = {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          Authorization:
+            'token ' +
+            JSON.parse(localStorage.getItem('userData') as string).token,
+        };
+      }
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/api/v1/products/create`,
+        {
+          method: 'POST',
+          headers,
+          body: JSON.stringify({
+            name: item.itemName,
+            amount: 1,
+          }),
+        }
+      );
+      if (response.status === 201) {
+        await response.json();
+        setItems([...items, item]);
+        setItem({
+          itemName: '',
+          visible: true,
+        });
+      }
     } catch (err) {}
   };
 
@@ -46,22 +113,17 @@ const Items: FC = () => {
 
   const searchItems = (event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
-    const arr = items.map((value) => {
-      // @ts-ignore
-      const pattern = new RegExp(event.target.value, 'g');
-      if (value.itemName.match(pattern)) {
-        value.visible = true;
-        return value;
-      } else {
-        value.visible = false;
-        return value;
-      }
-    });
-
+    // const arr = items.map((value) => {
+    //   const pattern = new RegExp(event.target.value, 'g');
+    //   value.itemName.match(pattern)
+    //     ? (value.visible = true)
+    //     : (value.visible = false);
+    //   return value;
+    // });
+    const arr = filterItemsByRegExp(items, event.target.value);
     setItems(arr);
   };
 
-  // @ts-ignore
   return (
     <div className='content'>
       <h3>Your Items</h3>
@@ -85,12 +147,15 @@ const Items: FC = () => {
 
       <form>
         <input
+          style={{ marginTop: '25px', marginBottom: '25px' }}
           type='text'
           value={search}
           name='search'
+          placeholder='Search item(s)'
           onChange={searchItems}
         />
       </form>
+
       <ul>
         {items &&
           items.map((value: ItemType) => {
