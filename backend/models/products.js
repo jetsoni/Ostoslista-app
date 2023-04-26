67650a555567514ffb61b811f84460f7c4ae225d
alari777 <alari777@gmail.com>
2023-04-26 09:09:02 +0300

    updated `Product` model: added `userId`

--- backend/models/products.js
+++ backend/models/products.js
@@ -2,7 +2,8 @@ const { Schema, model, Types } = require("mongoose");
 
 const schema = new Schema({
   name: { type: String, required: true, unique: true },
-  amount: { type: Number, required: true }
+  amount: { type: Number, required: true },
+  userId: { type: Types.ObjectId, ref: "Users" },
 });
 
 module.exports = model("Products", schema);
